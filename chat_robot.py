cebe77072b4883b17599da8c103dd20f7865e122
Devil-guigui <wang507811210@gmail.com>
2019-06-29 14:22:18 +0800

    第一次提交，实现了get_robot_reply智能聊天函数

---
+++ chat_robot.py
@@ -0,0 +1,51 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*--
+
+import urllib.request
+import urllib.parse
+import os
+
+#os.system("color 5F")
+os.system("title 聊天机器人小程序 By:鬼鬼")
+
+def get_robot_replay(question):
+    '''
+    函数功能：对于特定问题进行特定回复，对于非特定问题进行智能回复
+
+    参数描述：
+    question 聊天内容或问题
+
+    返回值：str,回复内容
+    '''
+
+    if "你叫什么名字" in question:
+        answer = "我叫鬼鬼"
+    elif "你多少岁" in question:
+        answer = "18岁哦"
+    elif "你是GG还是MM" in question:
+        answer = "你猜"
+    else:
+        try:
+            # 调用NLP接口实现智能回复
+            parms = urllib.parse.urlencode({'msg': question}).encode()  # 接口参数需要进行URL编码
+            req = urllib.request.Request("http://api.itmojun.com/chat_robot",parms,method="POST")  #创建
+            answer = urllib.request.urlopen(req).read().decode()  # 调用接口，（即向目标服务器发出HTTP请求，并获取服务器的响应数据）
+        except Exception as e:
+            answer = "AI机器人出现故障！（原因：%s）" % e
+    return answer
+
+if __name__ == '__main__':
+    # 测试get_robot_replay函数
+    # print(get_robot_replay("你叫什么名字"))
+    # print(get_robot_replay("武汉天气如何"))
+    # print(get_robot_replay("你到底是谁"))
+    # print(get_robot_replay("你爸爸是谁"))
+    # print(get_robot_replay("叫爸爸"))
+    print("请输出任何内容开始和机器人聊天：\n")
+    while True:
+        a = input("")
+        print(get_robot_replay(a))
+
+
+
+
